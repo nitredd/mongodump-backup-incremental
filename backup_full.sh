31ef991485b2f09b34b16c2519baef95d973e737
Nitin Reddy <82951937+nitredd@users.noreply.github.com>
2026-09-16 14:39:00 +0400

    backup_full.sh can now backup all collections within a specified database

--- backup_full.sh
+++ backup_full.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-MONGO_URI="mongodb://localhost"
+MONGO_URI="mongodb://localhost"  # Use /?directConnection=true to target a specific server
 DB_NAME="hr"
-COLL_NAME=("people" "departments")
+# COLL_NAME=("people" "departments")  # Our full snapshot can include all collections
 
 # Date suffix in yyyymmdd format
 DATE_SUFFIX=$(date +"%Y%m%d")
 
-# Iterate through array elements (using "${COLL_NAME[@]}")
-for ITERCOLL in "${COLL_NAME[@]}"
-do
+# # Iterate through array elements (using "${COLL_NAME[@]}")
+# for ITERCOLL in "${COLL_NAME[@]}"
+# do
     # TODO 2: Add timestamp suffix in yyyymmdd format to filename
+    ITERCOLL=all
     FILENAME="/backup/${DB_NAME}_${ITERCOLL}_${DATE_SUFFIX}_full.archive.gz"
 
     # Run mongodump (using $ITERCOLL for the collection)
     mongodump \
         --uri="$MONGO_URI" \
         --db="$DB_NAME" \
-        --collection="$ITERCOLL" \
+#         --collection="$ITERCOLL" \
         --archive="$FILENAME" \
         --readPreference=secondary \
         --gzip
@@ -28,4 +29,4 @@ do
     COLL: ${ITERCOLL}
     ARCHIVE: ${FILENAME}    
     "
-done
+# done
